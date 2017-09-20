06d4203289f0a7f4ee62a8c4b07183f2e843e3a8
Delta <deltaroboticsftc@gmail.com>
2017-09-19 20:51:32 -0500

    -Changed game variable -Started settings fragment

---
+++ app/src/main/java/org/deltaroboticsftc/relicrecovery17_18/fragmentSettings.java
@@ -0,0 +1,14 @@
+package org.deltaroboticsftc.relicrecovery17_18;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Luke Poellet on 9/19/2017.
+ */
+
+public class fragmentSettings extends Fragment
+{
+
+
+
+}
